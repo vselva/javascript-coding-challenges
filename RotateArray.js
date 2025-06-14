90defd209159cde810e6977e170beb9845f7d536
Selvakumar <vselva@gmail.com>
2025-06-14 21:58:52 +0530

    Update RotateArray.js

--- RotateArray.js
+++ RotateArray.js
@@ -1,15 +1,30 @@
 console.log('Rotate Array using build-in functions:');
-function rotateArray(inArr, count) {
-    let rotatedArray = [];
+function rotateArrayWithBuildInFunction(inArr, count) {
+    let rotated = [];
     for(let i = 0; i < count; i++) {
-        rotateArray = [inArr.pop(), ...inArr];
-        inArr = rotateArray;
-        console.log('roated:', rotateArray);
+        rotated = [inArr.pop(), ...inArr];
+        inArr = rotated;
+        console.log('roated:', rotated);
     }
 
-    return rotateArray;
+    return rotated;
 }
-const inArr = [1, 2, 3, 4, 5];
-console.log(`Rotating an Array [ ${inArr} ] = ${rotateArray(inArr, 2)}`);
+let inArr = [1, 2, 3, 4, 5];
+console.log(`Rotating an Array [ ${inArr} ] = ${rotateArrayWithBuildInFunction(inArr, 2)}`);
 
 console.log('');
+console.log('rotateArray without buildin functions');
+const rotateArray = (inArr, count) => {
+    let rotated = [];
+    for(let c = 0; c < count; c++) {
+        rotated = [];
+        rotated[0] = inArr[inArr.length-1];
+        for(let i = 1; i < inArr.length; i++) {
+            rotated[i] = inArr[i - 1];
+        }
+        inArr = rotated;
+    }
+    return rotated;
+}
+inArr = [1, 2, 3, 4, 5];
+console.log('Rotated Array:', rotateArray(inArr, 2));
