2b8709ddf744dbcbc68ce37fbf8da6264c04d709
Selvakumar <vselva@gmail.com>
2025-07-16 16:18:02 +0530

    Update IsPalindrome-builldin.js

--- IsPalindrome-builldin.js
+++ IsPalindrome-builldin.js
@@ -1,10 +1,27 @@
-const isPalindrome = (orgStr) => {
-  const cleanStr = orgStr.toLowerCase();
-  let revStr = cleanStr.split("").reverse().join("");
-  console.log(revStr);
-  return cleanStr === revStr
-    ? `✅ ${orgStr} is Palindrome`
-    : `❌ ${orgStr} is not Palindrome`;
+const isPalindrom = (string) => {
+  // checking whether it is a string
+  if (typeof string !== "string") {
+    return false;
+  }
+
+  // input validation
+  if (string === "" || string === null || string === undefined) {
+    return false;
+  }
+
+  // to lowercase
+  string = string.toLowerCase(string);
+
+  // remove non alpha numeric
+  string = string.replace(/[^a-z0-9]/gi, "");
+
+  // checking the reversed is same or not
+  const reversed = string.split("").reverse().join("");
+  if (string === reversed) {
+    return true;
+  }
+
+  return false;
 };
 
-console.log(isPalindrome("Madam"));
+console.log(isPalindrom("Was it a car or a cat I saw?"));
