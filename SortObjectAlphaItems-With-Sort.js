58e6e19bec0b2f0d529cc3f941fbe50e642d0146
Selvakumar <vselva@gmail.com>
2025-07-17 16:41:02 +0530

    sorting

---
+++ SortObjectAlphaItems-With-Sort.js
@@ -0,0 +1,14 @@
+// sort by object items
+const sortObjectItem = (obj) => {
+    const sorted = obj.sort((el1, el2) => el1.name.localeCompare(el2.name));
+    return sorted;
+};
+
+const myObj = [
+    { id: 1, name: "Selva" },
+    { id: 3, name: "Priya" },
+    { id: 2, name: "Arun" },
+    { id: 5, name: "Kumar" },
+    { id: 4, name: "Meena" },
+];
+sortObjectItem(myObj).map((e) => console.log(e.id, e.name));
