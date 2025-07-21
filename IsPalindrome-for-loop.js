4ac614b56b37298be19f332c754d5a821c4a8f93
Selvakumar <vselva@gmail.com>
2025-07-21 13:47:45 +0530

    Update IsPalindrome-for-loop.js

--- IsPalindrome-for-loop.js
+++ IsPalindrome-for-loop.js
@@ -1,11 +1,12 @@
 function isPalindrome(string) {
-  string = string.toLowerCase();
-  length = string.length;
-  for (let i = 0; i < length / 2; i++) {
-    if (string[i] != string[length - i - 1]) return false;
-  }
-  return true;
+    string = string.toLowerCase();
+    string = string.replace(/[^a-z0-9]/gi, ""); // cleanup
+    length = string.length;
+    for (let i = 0; i < length / 2; i++) {
+        if (string[i] != string[length - i - 1]) return false;
+    }
+    return true;
 }
 
-console.log(isPalindrome("Madam"));
-console.log(isPalindrome("Maddam"));
+console.log(isPalindrome("Mad am?"));
+console.log(isPalindrome("Maddamy"));
