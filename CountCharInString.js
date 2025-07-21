6c5762d0aced536a629e2bc26693db0ecb409a6c
Selvakumar <vselva@gmail.com>
2025-07-21 13:57:36 +0530

    Update CountCharInString.js

--- CountCharInString.js
+++ CountCharInString.js
@@ -1,30 +1,11 @@
-// Function to count occurrences of a character in a string without using .length
-function findCountOfCharInString(inStr, inChar) {
-    let countOfChar = 0;
-
-    // Loop through the string using for...in to avoid using .length
-    for (let i in inStr) {
-        if (inStr[i] === inChar) {
-            countOfChar++;
-        }
+const charCountInString = (string) => {
+    let charMap = {};
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i];
+        charMap[char] = charMap[char] ? ++charMap[char] : 1;
     }
 
-    return countOfChar;
-}
+    return charMap;
+};
 
-let inStr = '122333444455555666666';
-let printedChar = {}; // To track already processed characters
-
-// Loop through string without using .length
-for (let i in inStr) {
-    let char = inStr[i];
-
-    // If character not yet counted, process it
-    if (!printedChar[char]) {
-        let count = findCountOfCharInString(inStr, char);
-        console.log(`The occurrence of '${char}' in "${inStr}" is ${count}`);
-        printedChar[char] = count; // Mark character as processed
-    }
-}
-console.log('-------------------------------------------------------');
-console.log(printedChar);
+console.log(charCountInString("122333444455555666666"));
