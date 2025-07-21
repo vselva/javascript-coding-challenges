c64b6b2283453ca011533b21db1fdea5394e994d
Selvakumar <vselva@gmail.com>
2025-07-21 13:48:59 +0530

    Update Fibonacci-Series-for-loop.js

--- Fibonacci-Series-for-loop.js
+++ Fibonacci-Series-for-loop.js
@@ -1,11 +1,13 @@
 function fibonacci(n) {
-  if (n <= 1) return 1;
-  fib = [0, 1];
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-  }
+    if (n <= 1) return 1;
 
-  return fib;
+    fib = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib;
 }
 console.log(fibonacci(0));
 console.log(fibonacci(1));
