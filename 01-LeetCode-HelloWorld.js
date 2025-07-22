0193aabc307949aae2f3ba13ea29b95932f6cbcb
Selvakumar <vselva@gmail.com>
2025-07-22 12:20:47 +0530

    Update 01-LeetCode-HelloWorld.js

--- 01-LeetCode-HelloWorld.js
+++ 01-LeetCode-HelloWorld.js
@@ -9,7 +9,20 @@ function returnHello() {
         return "Hello World";
     };
 }
-
 const f = returnHello();
 const ret = f();
 console.log(ret);
+
+// returning named function
+function returnHello1() {
+    function hello() {
+        return "Hello World";
+    }
+    return { hello };
+}
+// without destructuring the object returned
+const f1 = returnHello1();
+console.log(f1.hello());
+// destructuring the object returned
+const { hello } = returnHello1();
+console.log(hello());
