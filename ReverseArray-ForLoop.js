09c8f2ae3c69b4814cdc1f97bb77ff9fca6f63c1
Selvakumar <vselva@gmail.com>
2025-07-28 11:21:05 +0530

    Refactor reverse array function and clean up code
    Renamed the function from reverseArray to reverse, updated parameter naming, and switched from push to direct assignment for reversing. Also removed commented code and unused variables for clarity.

--- ReverseArray-ForLoop.js
+++ ReverseArray-ForLoop.js
@@ -1,13 +1,10 @@
-const reverseArray = (array) => {
+const reverse = (arr) => {
     let reversed = [];
     let j = 0;
-    const length = array.length;
-
-    for (let i = length - 1; i >= 0; i--) {
-        //reversed[j++] = array[i];
-        reversed.push(array[i]);
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed[j++] = arr[i];
     }
     return reversed;
 };
 
-console.log(reverseArray([1, 2, 3, 4, 5]));
+console.log(reverse([1, 2, 3, 4, 5]));
