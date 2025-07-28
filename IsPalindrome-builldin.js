0010a3cbad1b9ed3a3a622128694a00dbb9c1922
Selvakumar <vselva@gmail.com>
2025-07-28 10:49:42 +0530

    Refactor and simplify palindrome function
    Renamed the function to isPalindrome, removed redundant input validation, and streamlined the logic for checking palindromes. Updated console.log statements to use the new function name and test cases.

--- IsPalindrome-builldin.js
+++ IsPalindrome-builldin.js
@@ -1,28 +1,9 @@
-const isPalindrom = (string) => {
-  // checking whether it is a string
-  if (typeof string !== "string") {
-    return false;
-  }
-
-  // input validation
-  if (string === "" || string === null || string === undefined) {
-    return false;
-  }
-
-  // to lowercase
-  string = string.toLowerCase(string);
-
-  // remove non alpha numeric
-  string = string.replace(/[^a-z0-9]/gi, "");
-
-  // checking the reversed is same or not
-  const reversed = string.split("").reverse().join("");
-
-  if (string === reversed) {
-    return true;
-  }
-
-  return false;
+const isPalindrome = (str) => {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-z0-9]/gi, "");
+    rev = str.split("").reverse().join("");
+    return str === rev;
 };
 
-console.log(isPalindrom("Was it a car or a cat I saw?"));
+console.log(isPalindrome("ma da?m"));
+console.log(isPalindrome("madam1"));
