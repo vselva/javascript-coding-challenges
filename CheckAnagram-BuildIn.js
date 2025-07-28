ff8034b5cdc3f4a6ede6cd179f01d1bc1f4b08e4
Selvakumar <vselva@gmail.com>
2025-07-28 08:48:25 +0530

    Remove CheckAnagram-BuildIn.js file
    Deleted the CheckAnagram-BuildIn.js file, which contained a function to check if two strings are anagrams using built-in methods. This cleanup may be part of refactoring or removing unused code.

--- CheckAnagram-BuildIn.js
+++
@@ -1,9 +0,0 @@
-const checkAnagram = (str1, str2) => {
-    let str1sorted = str1.split("").sort().join("");
-    let str2sorted = str2.split("").sort().join("");
-
-    return str1sorted === str2sorted;
-};
-
-console.log(checkAnagram("madam", "mdmaa"));
-console.log(checkAnagram("madam", "mdma"));
