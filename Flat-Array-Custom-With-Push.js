01bda6e9d1fd320d3e4d04a585b99377532533f4
Selvakumar <vselva@gmail.com>
2025-07-28 08:40:12 +0530

    Reformat customFlat function with consistent indentation
    Updated the customFlat function to use consistent 4-space indentation for improved readability. No functional changes were made.

--- Flat-Array-Custom-With-Push.js
+++ Flat-Array-Custom-With-Push.js
@@ -1,18 +1,18 @@
 customFlat = (inArr) => {
-  let outArr = [];
-  let j = 0;
-  for (let i = 0; i < inArr.length; i++) {
-    if (Array.isArray(inArr[i])) {
-      // outArr.push(...inArr[i]);
-      for (let k = 0; k < inArr[i].length; k++) {
-        outArr[j++] = inArr[i][k];
-      }
-    } else {
-      outArr[j++] = inArr[i];
-      // outArr.push(inArr[i]);
+    let outArr = [];
+    let j = 0;
+    for (let i = 0; i < inArr.length; i++) {
+        if (Array.isArray(inArr[i])) {
+            // outArr.push(...inArr[i]);
+            for (let k = 0; k < inArr[i].length; k++) {
+                outArr[j++] = inArr[i][k];
+            }
+        } else {
+            outArr[j++] = inArr[i];
+            // outArr.push(inArr[i]);
+        }
     }
-  }
-  return outArr;
+    return outArr;
 };
 
 const inArr = [1, 2, [3, 4], [5, 6]];
